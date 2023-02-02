1b34943a3bb944a16e41e7e36008fdcc10a4bae3
Akanksha <akanksha03@nagarro.com>
2023-02-02 12:35:23 +0530

    changes in code

--- JenkinsWithGit/Controllers/WeatherForecastController.cs
+++ JenkinsWithGit/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace JenkinsWithGit.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            // do some changes
         }
 
         [HttpGet]
